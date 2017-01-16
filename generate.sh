ad114711e8df4a38c4d3903eda921232914c2689
JohnDDuncanIII <duncjo01@gettysburg.edu>
2017-01-16 14:07:09 -0500

    add shell script to create files/folders and set needed permissions

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir archives/
+touch index.html
+touch archives/index.html
+chmod 666 cgi-bin/gm-counter.cgi
+chmod 666 cgi-bin/gm-entrylist.cgi
+chmod 666 cgi-bin/gm-cplog.cgi
+chmod 666 cgi-bin/gm-authors.cgi
+chmod 666 cgi-bin/gm-templates.cgi
+chmod 666 cgi-bin/gm-banlist.cgi
+chmod 666 cgi-bin/gm-config.cgi
+chmod 777 archives/
+chmod 666 index.html
+chmod 666 archives/index.html
